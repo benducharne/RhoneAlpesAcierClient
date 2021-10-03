f06046a60c12462feffc2b3cdc8ae22ff6339b24
benducharne <benjamin.ducharne@gmail.com>
2021-10-03 23:27:15 +0200

    ready to deploy

--- src/components/realisations/IntroOverlay/IntroOverlay.js
+++ src/components/realisations/IntroOverlay/IntroOverlay.js
@@ -1,18 +1,36 @@
 import React from "react";
 import "./IntroOverlay.scss";
+import { useViewport } from "../../../assets/hooks/useViewport";
 
-const HoIntroOverlay = () => {
+const IntroOverlay = () => {
+  const { height } = useViewport();
+
   return (
     <>
-      <div className="white-intro-overlay" />
+      <div
+        className="white-intro-overlay"
+        style={{
+          height: `${height}px`,
+        }}
+      />
 
       <div className="black-intro-overlay">
-        <div className="top">
+        <div
+          className="top"
+          style={{
+            height: `${height / 2}px`,
+          }}
+        >
           <div className="overlay-top"></div>
           <div className="overlay-top"></div>
           <div className="overlay-top"></div>
         </div>
-        <div className="bottom">
+        <div
+          className="bottom"
+          style={{
+            height: `${height / 2}px`,
+          }}
+        >
           <div className="overlay-bottom"></div>
           <div className="overlay-bottom"></div>
           <div className="overlay-bottom"></div>
@@ -22,4 +40,4 @@ const HoIntroOverlay = () => {
   );
 };
 
-export default HoIntroOverlay;
+export default IntroOverlay;
